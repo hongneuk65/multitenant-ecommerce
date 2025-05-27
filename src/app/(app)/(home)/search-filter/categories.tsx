e7552b3d4ee058fd220eafd33efc9787611bc288
hongneuk65 <vanhonga3@gmail.com>
2025-05-27 11:16:20 +0700

    nghi trua

---
+++ src/app/(app)/(home)/search-filter/categories.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    data: any;
+};
+
+export const Categories = ({data}:Props) => {
+    return (
+        <div>
+         Categories :{JSON.stringify(data, null, 2)}
+        </div>
+    )
+};
