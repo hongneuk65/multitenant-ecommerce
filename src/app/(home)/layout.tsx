aa486a56fe1729f5a6294958e38cbb0264404524
hongneuk65 <vanhonga3@gmail.com>
2025-05-27 07:58:56 +0700

    suafooter

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,16 +3,16 @@ import { Navbar } from "./navbar";
 
 interface Props {
     children: React.ReactNode;
-};
+}
 
-const Layout = ({children}:Props) => {
+const Layout = ({ children } : Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <div className="flex">
-            {children}
+            <div className="flex-1 bg-[#F4F4F0]">
+                {children} 
             </div>
-            <Footer/>
+                <Footer />
         </div>
     );
 }
